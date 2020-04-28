521ea60f95ec3abd3198e1f11c0ea89c13ddb1b9
Michot Julien <julien.michot@hotmail.fr>
2020-04-28 07:12:57 +0000

    Update MyResourceTest.java

--- src/test/java/fr/ulille/iut/agile/MyResourceTest.java
+++ src/test/java/fr/ulille/iut/agile/MyResourceTest.java
@@ -43,6 +43,6 @@ public class MyResourceTest {
     @Test
     public void testGetIt() {
         String responseMsg = target.path("myresource").request().get(String.class);
-        assertEquals("Got it!", responseMsg);
+        assertEquals("Hello World !", responseMsg);
     }
 }
