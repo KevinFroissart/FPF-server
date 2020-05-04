0e21bfd8cd516ad7effc546b6439acd72ce4b339
Aymeric Hénouille <aymeric.henouille@gmail.com>
2020-05-04 23:46:44 +0200

    feat: database tables

---
+++ src/main/java/fr/fairepartfait/server/controllers/CardShapeController.java
@@ -0,0 +1,26 @@
+package fr.fairepartfait.server.controllers;
+
+import fr.fairepartfait.server.models.CardShape;
+import fr.fairepartfait.server.repositories.CardShapeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class CardShapeController {
+
+    @Autowired
+    private CardShapeRepository cardShapeRepository;
+
+    @GetMapping("cardshapes")
+    public @ResponseBody Iterable<CardShape> getCardShapes() {
+        return cardShapeRepository.findAll();
+    }
+
+    @GetMapping("cardshapes/{id}")
+    public @ResponseBody CardShape getCardShapeById(@PathVariable int id) {
+        return cardShapeRepository.existsById(id) ? cardShapeRepository.findById(id).get() : null;
+    }
+
+
+}
